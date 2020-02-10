5a07ba7713156a3dc5489a8b605cc00422796ac2
lalyperra <35471457+lalyperra@users.noreply.github.com>
2020-02-09 17:34:27 -0800

    update main.js script
    clarify main.js folder and update js to produce console log correctly

---
+++ main.js
@@ -0,0 +1,24 @@
+var pizza = {
+  crust: [
+      'pan',
+      'thin',
+      'hand-tossed'
+    ],
+  cheese: [
+      'regular',
+      'extra'
+    ],
+  sauce: [
+      'marinara',
+      'alfredo',
+      'bbq'
+    ],
+  addToppings:[
+    console.log('Please add pepperoni and sausage')
+  ]
+}
+
+   console.log(pizza.crust[0]),
+   console.log(pizza.cheese[1]),
+   console.log(pizza.sauce[0]),
+   console.log(pizza.addToppings);
